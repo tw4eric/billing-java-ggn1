ebed910839c35419827330b386850cfa11b15a3b
Ravindra R. Jaju <jaju@msync.org>
2011-10-19 14:23:46 +0530

    Some impl. in file-based reader and the rawrecord struct.

--- src/tw/eric/billing/FileRawRecordsReader.java
+++ src/tw/eric/billing/FileRawRecordsReader.java
@@ -1,10 +1,21 @@
 package tw.eric.billing;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileRawRecordsReader implements RawRecordsReader {
+	
+	public FileRawRecordsReader(String filename) throws FileNotFoundException {
+		File file = new File(filename);
+		if (!file.exists())
+			throw new FileNotFoundException();
+	}
+	
 	public List<RawRecord> getRecords()
 	{
-		return null;
+		List<RawRecord> retVal = new ArrayList<RawRecord>();
+		return retVal;
 	}
 }
--- src/tw/eric/billing/RawRecord.java
+++ src/tw/eric/billing/RawRecord.java
@@ -1,18 +1,65 @@
 package tw.eric.billing;
 
 public class RawRecord {
-	private String mobileNumber;
-	private long callId;
-	public enum CallType{Local,STD,ISD};
-	private long startTime;
-	private long endTime;
-	public enum Period{Start,Continuation,End};
+
+	public enum CallType { Local, STD, ISD };
+	public enum Period   { Start, Continuation, End };
 	
-	public RawRecord(String mobileNumber,long callId,CallType callType,long startTime,long endTime,Period period)
-	{
-		
+	private final String mobileNumber;
+	private final long callId;
+	private final CallType callType;
+	private final long startTime;
+	private final long endTime;
+	private final Period period;
+	
+	public String getMobileNumber() {
+		return mobileNumber;
 	}
 
+	public long getCallId() {
+		return callId;
+	}
 
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+
+	public CallType getCallType() {
+		return callType;
+	}
+
+	public Period getPeriod() {
+		return period;
+	}
+
+	public RawRecord(String mobileNumber,
+			String callId,
+			String callType,
+			String startTime,
+			String endTime,
+			String period)
+	{
+		this(mobileNumber, Long.parseLong(callId), CallType.valueOf(callType),
+				Long.parseLong(startTime), Long.parseLong(endTime), Period.valueOf(period));
+	}
+	
+	public RawRecord(String mobileNumber,
+			long callId,
+			CallType callType,
+			long startTime,
+			long endTime,
+			Period period)
+	{
+		this.mobileNumber = mobileNumber;
+		this.callId = callId;
+		this.callType = callType;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.period = period;
+	}
 
 }
